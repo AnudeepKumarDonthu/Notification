b9f3734126bf292a62aa7ad88745d18822bda6a5
Anudeep Kumar Donthu <anudeepdonthu@gmail.com>
2018-04-19 07:26:39 +0530

    2nd commit

--- src/main/java/com/friends/controller/MainController.java
+++ src/main/java/com/friends/controller/MainController.java
@@ -1,67 +1,105 @@
 /**
- * 
+
+*
+
  */
+
 package com.friends.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.friends.service.MainService;
 
 /**
+ * 
  * @author AnudeepKumar
  *
+ * 
+ * 
  */
+
 @RestController
+
 public class MainController {
 
 	@Autowired
+
 	@Qualifier("mainService")
+
 	private MainService mainService;
 
-	@RequestMapping(value = "/Ping")
+	@RequestMapping(value = "/ping")
+
 	public String pingMe() {
+
 		return "I am available!!!";
+
 	}
 
 	@RequestMapping(value = "/sendmorningcoffeeemail")
+
 	public String sendMorningCoffeeEmail() {
+
 		try {
+
 			mainService.workOnSendsendMorningCoffeeEmail();
+
 			return "**********Morning Coffee Email Sent Successfully**********";
+
 		} catch (Exception exception) {
+
 			System.out.println(exception);
+
 			return "**********Email Sending Failed********** :: " + exception;
+
 		}
 
 	}
-	
+
 	@RequestMapping(value = "/sendeveningcoffeeemail")
+
 	public String sendEveningCoffeeEmail() {
+
 		try {
+
 			mainService.workOnSendsendEveningCoffeeEmail();
+
 			return "**********Evening Coffee Email Sent Successfully**********";
+
 		} catch (Exception exception) {
+
 			System.out.println(exception);
+
 			return "**********Email Sending Failed********** :: " + exception;
+
 		}
 
 	}
-	
-	
+
 	@RequestMapping(value = "/sendlunchemail")
+
 	public String sendLunchEmail() {
+
 		try {
+
 			mainService.workOnSendingLunchEmail();
+
 			return "**********Lunch Email Sent Successfully**********";
+
 		} catch (Exception exception) {
+
 			System.out.println(exception);
+
 			return "**********Email Sending Failed********** :: " + exception;
+
 		}
 
 	}
-	
-	
+
 }
